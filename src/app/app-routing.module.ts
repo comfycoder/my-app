37517d24289d8713cb57134be24c2d0ae2346534
Dennis Moon <dmoon396@hotmail.com>
2019-07-20 11:08:25 -0500

    Added many new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AccountListComponent } from './account-list/account-list.component';
+import { AccountCreateComponent } from './account-create/account-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { ActivityCreateComponent } from './activity-create/activity-create.component';
+
+const routes: Routes = [
+  { path:  '', redirectTo:  'contacts', pathMatch:  'full' },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'accounts',
+    component: AccountListComponent
+  },
+  {
+    path: 'create-account',
+    component: AccountCreateComponent
+  },
+  {
+    path: 'contacts',
+    component: ContactListComponent
+  },
+  {
+    path: 'create-contact',
+    component: ContactCreateComponent
+  },
+  {
+    path: 'activities',
+    component: ActivityListComponent
+  },
+  {
+    path: 'create-activity',
+    component: ActivityCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from  '@angular/common/http';
+import { HttpClientXsrfModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { 
+  MatToolbarModule, 
+  MatIconModule, 
+  MatSidenavModule, 
+  MatListModule, 
+  MatButtonModule 
+} from  '@angular/material';
+
+import { ConfigComponent }      from './config/config.component';
+
+import { AccountListComponent } from './account-list/account-list.component';
+import { AccountCreateComponent } from './account-create/account-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { ActivityCreateComponent } from './activity-create/activity-create.component';
+import { HomeComponent } from './home/home.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigComponent,
+    AccountListComponent,
+    AccountCreateComponent,
+    ContactListComponent,
+    ContactCreateComponent,
+    ActivityListComponent,
+    ActivityCreateComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HttpClientXsrfModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
     AppRoutingModule
   ],
   providers: [],
