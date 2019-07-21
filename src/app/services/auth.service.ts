028ee4579572ba8b2a61d93599990180df68ed04
Dennis Moon <dmoon396@hotmail.com>
2019-07-21 10:01:15 -0500

    Fixed service file references

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 
 /** Mock client-side authentication/authorization service */
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthService {
   getAuthorizationToken() {
     return 'some-auth-token';
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,10 +1,53 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from  '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpHeaders } from "@angular/common/http";
+
+import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
+
+import { Contact } from "../models/contact";
+import {
+  HttpErrorHandler,
+  HandleError
+} from "../services/http-error-handler.service";
+import { Config } from "../models/config";
+import { ConfigService } from "./config.service";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json",
+    Authorization: "my-auth-token"
+  })
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ContactService {
+  config: Config;
+  contactsUrl = ""; // URL to web api
+  private handleError: HandleError;
 
-  constructor(private  httpClient:  HttpClient) { }
+  constructor(
+    private configService: ConfigService,
+    private httpClient: HttpClient,
+    httpErrorHandler: HttpErrorHandler
+  ) {
+    this.handleError = httpErrorHandler.createHandleError("ContactService");
+    this.configService
+      .getConfigResponse()
+      // resp is of type `HttpResponse<Config>`
+      .subscribe(resp => {
+        // access the body directly, which is typed as `Config`.
+        this.config = { ...resp.body };
+        this.contactsUrl = this.config.contactsUrl;
+      });
+  }
+
+  /** GET contacts from the server */
+  getContacts(): Observable<Contact[]> {
+    return this.httpClient
+      .get<Contact[]>(this.contactsUrl)
+      .pipe(catchError(this.handleError("getContacts", [])));
+  }
 }
