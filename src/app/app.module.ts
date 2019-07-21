7f34eb64fa3575eea35a594c0211ebe052004628
Dennis Moon <dmoon396@hotmail.com>
2019-07-21 16:56:02 -0500

    Added new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { HttpClientModule } from  '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { HttpClientXsrfModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { 
-  MatToolbarModule, 
-  MatIconModule, 
-  MatSidenavModule, 
-  MatListModule, 
-  MatButtonModule, 
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatButtonModule,
   MatTableModule,
   MatPaginatorModule,
   MatFormFieldModule,
   MatInputModule,
-  MatSortModule
-} from  '@angular/material';
+  MatSortModule,
+  MatCardModule
+} from '@angular/material';
 
-import { ConfigComponent }      from './config/config.component';
-import { MessageService }       from './services/message.service';
-import { MessagesComponent }    from './messages/messages.component';
+import { ChartsModule } from 'ng2-charts';
+import { NgxSpinnerModule } from "ngx-spinner";
+
+import { ConfigComponent } from './config/config.component';
+import { MessageService } from './services/message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 import { AccountListComponent } from './account-list/account-list.component';
 import { AccountCreateComponent } from './account-create/account-create.component';
@@ -31,6 +35,12 @@ import { ContactCreateComponent } from './contact-create/contact-create.componen
 import { ActivityListComponent } from './activity-list/activity-list.component';
 import { ActivityCreateComponent } from './activity-create/activity-create.component';
 import { HomeComponent } from './home/home.component';
+import { MyLineChartComponent } from './my-line-chart/my-line-chart.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { LayoutModule } from '@angular/cdk/layout';
+import { PackageSearchComponent } from './package-search/package-search.component';
 
 
 @NgModule({
@@ -45,6 +55,9 @@ import { HomeComponent } from './home/home.component';
     ActivityListComponent,
     ActivityCreateComponent,
     HomeComponent,
+    MyLineChartComponent,
+    DashboardComponent,
+    PackageSearchComponent
   ],
   imports: [
     BrowserModule,
@@ -61,7 +74,13 @@ import { HomeComponent } from './home/home.component';
     MatPaginatorModule,
     MatFormFieldModule,
     MatInputModule,
-    AppRoutingModule
+    MatCardModule,
+    ChartsModule,
+    NgxSpinnerModule,
+    AppRoutingModule,
+    MatGridListModule,
+    MatMenuModule,
+    LayoutModule
   ],
   providers: [
     MessageService
